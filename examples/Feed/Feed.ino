e0aff4e6bd4af0992bf8dfe4f6931977ce9cb043
Chavee Issariyapat <i@chavee.com>
2017-03-27 12:50:39 +0700

    add Feed example

---
+++ examples/Feed/Feed.ino
@@ -0,0 +1,74 @@
+#include <Ethernet.h>
+#include <MicroGear.h>
+
+#define APPID   <APPID>
+#define KEY     <APPKEY>
+#define SECRET  <APPSECRET>
+#define ALIAS   "feedsender"
+
+#define FEEDID  <FEEDID>
+#define FEEDAPIKEY  <FEEDAPIKEY>
+
+EthernetClient client;
+AuthClient *authclient;
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEA };
+MicroGear microgear(client);
+int timer = 0;
+String feedstr;
+
+void onErrorHandler(char *topic, uint8_t* msg, unsigned int msglen) {
+  Serial.print("\nError --> ");
+  msg[msglen] = '\0';
+  Serial.println((char *)msg);
+}
+
+void onInfoHandler(char *topic, uint8_t* msg, unsigned int msglen) {
+  Serial.print("\nInfo --> ");
+  msg[msglen] = '\0';
+  Serial.println((char *)msg);
+}
+
+void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) {
+  Serial.println("Connected to NETPIE...");
+  microgear.setAlias(ALIAS);
+}
+
+void setup() {  
+    Serial.begin(9600);
+    Serial.println("Starting...");
+
+    microgear.on(ERROR,onErrorHandler);   //Call this if you want to see error message
+    microgear.on(INFO,onInfoHandler);     //Call this if you want to see info message
+    microgear.on(CONNECTED,onConnected);
+
+    if (Ethernet.begin(mac)) {
+      Serial.println(Ethernet.localIP());
+      microgear.init(KEY,SECRET,ALIAS);
+      microgear.connect(APPID);
+    }
+}
+
+void loop() {
+  if (microgear.connected()) {
+    microgear.loop();
+    Serial.print(".");
+    if (timer >= 15000) {
+        // generate a demo value of sine wave
+        feedstr = String("value:")+String(round(100.0*sin(2*3.1416*millis()/240000)));
+        Serial.print("\nwrite to feed -- >"); Serial.println(feedstr);
+        microgear.writeFeed(FEEDID,feedstr,FEEDAPIKEY);
+        timer = 0;
+      }
+      else timer += 1000;
+  }
+  else {
+      Serial.println("connection lost, reconnect...");
+      if (timer >= 5000) {
+          microgear.connect(APPID);
+      timer = 0;
+    }
+    else timer += 1000;
+  }
+  delay(1000);
+}
